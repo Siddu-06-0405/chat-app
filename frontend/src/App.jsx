cccd31376152d9681142395063c71bd33478052e
siddu <csiddhardha0@gmail.com>
2024-09-07 11:41:59 +0530

    upgraded the replicant Interface

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,10 @@
 //background img 1920 x 1080
 import './App.css'
 import Home from './pages/home/home'
-import Login from './pages/login/Login'
-import SignUp from './pages/signup/SignUp'
 
 function App() {
   return (
     <div className='p-4 h-screen flex items-center justify-center'>
-      {/* <SignUp/> */}
-      {/* <Login /> */}
       <Home />
     </div>
   )
